1bd9df12a8683075c598396298d962d9d5081b48
CIANCI BENJAMIN <benjamin.cnc@localhost.localdomain>
2025-04-16 11:06:14 +0200

    YES

--- main.py
+++ main.py
@@ -13,15 +13,33 @@ o|.............|o
 # Constantes
 WHITE = 0
 BLACK = 1
+MOTOR_BACKWARD = 1
 
 # Variable globale
 Init = True
 
 def followLine(speed:int, speed_slow:int):
     
-    # On teste si les capteurs infrarouge frontaux L1 et R1 détecte la bande noire
-    if line_sensor(LineSensor.R1)== WHITE and line_sensor(LineSensor.R2)== BLACK:
-        # On est sur la bande noire, on continue tout droit.
+    if line_sensor(LineSensor.R2)== BLACK and line_sensor(LineSensor.M) == BLACK :
+        # On tourne 90° à gauche
+        display.show("L")
+        motor_stop()
+        utime.sleep_ms(500)
+        motor_run(Motor.RIGHT, speed)
+        motor_run(Motor.LEFT, speed, MOTOR_BACKWARD)
+        utime.sleep_ms(500)
+        
+    elif line_sensor(LineSensor.R2)== WHITE and line_sensor(LineSensor.L2) == WHITE :
+        # On tourne 90° à droite       
+        display.show("R")
+        motor_stop()
+        utime.sleep_ms(500)
+        motor_run(Motor.RIGHT, speed, MOTOR_BACKWARD)
+        motor_run(Motor.LEFT, speed)
+        utime.sleep_ms(500)
+        
+    elif line_sensor(LineSensor.R1)== WHITE and line_sensor(LineSensor.R2)== BLACK:
+        # On est entre les bandes noires, on continue tout droit.
         display.show(Image.HAPPY)
         motor_run(Motor.LEFT, speed)
         motor_run(Motor.RIGHT, speed)
@@ -37,18 +55,6 @@ def followLine(speed:int, speed_slow:int):
         display.show("G")
         motor_run(Motor.RIGHT, speed)
         motor_run(Motor.LEFT, speed_slow)
-    
-    elif line_sensor(LineSensor.R2)== BLACK and line_sensor(LineSensor.M) == BLACK :
-        display.show("L")
-        motor_run(Motor.RIGHT, speed)
-        motor_run(Motor.LEFT, speed, MOTOR_BACKWARD)
-        utime.sleep_ms(200)
-        
-    elif line_sensor(LineSensor.R2)== WHITE and line_sensor(LineSensor.L2) == WHITE :
-        display.show("R")
-        motor_run(Motor.RIGHT, speed, MOTOR_BACKWARD)
-        motor_run(Motor.LEFT, speed)
-        utime.sleep_ms(200)
         
     utime.sleep_ms(50)
 
@@ -56,10 +62,11 @@ while True:
     if Init:
         # Vitesse maximale des moteurs (min:0, max:255)
         speed:int = 70   #70
-        speed_slow:int = 15 #15
-        led_rgb(rbg(255,255,255))
-        display.show("1")
-        utime.sleep_ms(3000)
+        speed_slow:int = 35 #15
+        led_rgb(rgb(255,255,255))
+        for k in range(3,0,-1):
+            display.show(k)
+            utime.sleep_ms(500)
         Init = False
 
     followLine(speed, speed_slow)
