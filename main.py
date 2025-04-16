16af1003ea9c010d47f28597aa02ff9ffcb758ad
CIANCI BENJAMIN <benjamin.cnc@localhost.localdomain>
2025-04-16 11:24:29 +0200

    correction

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ MOTOR_BACKWARD = 1
 
 # Variable globale
 Init = True
+true = True
 
 def followLine(speed:int, speed_slow:int):
     
@@ -28,6 +29,7 @@ def followLine(speed:int, speed_slow:int):
         motor_run(Motor.RIGHT, speed)
         motor_run(Motor.LEFT, speed, MOTOR_BACKWARD)
         utime.sleep_ms(500)
+        led_rgb(rgb(255,255,255))
         
     elif line_sensor(LineSensor.R2)== WHITE and line_sensor(LineSensor.L2) == WHITE :
         # On tourne 90° à droite       
@@ -37,24 +39,28 @@ def followLine(speed:int, speed_slow:int):
         motor_run(Motor.RIGHT, speed, MOTOR_BACKWARD)
         motor_run(Motor.LEFT, speed)
         utime.sleep_ms(500)
+        led_rgb(rgb(255,255,255))
         
     elif line_sensor(LineSensor.R1)== WHITE and line_sensor(LineSensor.R2)== BLACK:
         # On est entre les bandes noires, on continue tout droit.
         display.show(Image.HAPPY)
         motor_run(Motor.LEFT, speed)
         motor_run(Motor.RIGHT, speed)
-
+        led_rgb(rgb(255,255,255))
+        
     elif line_sensor(LineSensor.R2) == WHITE and line_sensor(LineSensor.L2) == BLACK:        
         # On est sorti à gauche, il faut revenir un peu sur la droite.
         display.show("D") 
         motor_run(Motor.RIGHT, speed_slow)
         motor_run(Motor.LEFT, speed)
-
+        led_rgb(rgb(255,50,50))
+        
     elif line_sensor(LineSensor.R1) == BLACK:
         # On est sorti à droite, il faut revenir un peu sur la gauche.
         display.show("G")
         motor_run(Motor.RIGHT, speed)
         motor_run(Motor.LEFT, speed_slow)
+        led_rgb(rgb(255,0,0))
         
     utime.sleep_ms(50)
 
@@ -68,10 +74,12 @@ while True:
             display.show(k)
             utime.sleep_ms(500)
         Init = False
+        
     while true:
         followLine(speed, speed_slow)
         if ultrasonic() < 8:
             true = False
+            
     motor_stop()
     led_rgb(rgb(0,255,0))
     
