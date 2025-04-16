510443ef84b54bedb155b7843fe4658e117f2b59
Lucaccroupi-dans-les-buissons <luca.fllt@eduge.ch>
2025-04-16 11:24:07 +0200

    Add files via upload

---
+++ protocole_broadcast_luca_et_ben.py
@@ -0,0 +1,23 @@
+
+
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Access rights validated</title>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <script type="text/javascript" src="/ginasso/js/DomPurify/purify.min.js" defer></script>
+        <script type="text/javascript" src="/ginasso/js/saml2-write.js" defer></script>
+    </head>
+    <body>
+        <form method="post" action="">
+            <input type="hidden" id="saml2Request" value="eyJ0eXAiOiJKV1QiLCJ6aXAiOiJERUYiLCJraWQiOiJhbS5nbG9iYWwuc2VydmljZXMuc2FtbDIuY2xpZW50LnN0b3JhZ2Uuand0LmVuY3J5cHRpb24iLCJlbmMiOiJBMjU2R0NNIiwiYWxnIjoiZGlyIn0..9HojqIhZpaSuzi4C._yWCl3KPc-CKs0RgWznazul1nkGLetvnKKvldkaALEUWrdw57_ARMNK4bHi08ad3fhILNJmi1Pg_mLDvIWPwJphCfLMjJ2pKnrMbjHD53XhdZGqcIlB_DgwIAqlOOoqIOlSxu4J5fmORdwgZZX0nunvGkJ3eJDDw1GXw_Lw1_CrIum9F1buuAURl2oLSAs3C1cWGsMcihDjQWWbKWgIOrV9ssW5NWxDIVGwykz3NXAgtAdVDxWt3vDCFzQD0DEnJiajuqvgc_GPiZY4-nFQdB65f7-bQ2bAU5yhSwIs06XrKmnXG6kBUtUXsB6OsimqyJwZ19qTVDGMqdXQSHqspPylgIEagXtRiACXa2-k08VkxCW_3nEN3yPaVE2kC6YolvNKMJyTyshrTtbzK4wh0W0FWnPcgxF7PGVcUm_QBO7OKGba0PBaJgxMLAiUhj331w_rTBj4NZFYGXz9W7vqw9UOuN4Hpqx9yQRMfqMr1jjZMtS9arv-__khxCZlaNvl6q61R9ne362TxHBnG5kZ03kZ8XGZP9QM0M_Zwntg6I47y9muSXr9ZkljFQvO49xLmhXCZvMqHcH1ZWB4Dt5T0rH28QI9wFN9Hv6mPPbTCUpzNhGU--dhwuxL0SuyEl9SmdbeJGhHF2jkl6Z0Pmoz0YNSEnOwQnbU3iWHS77maiBn34eP_5xSznv2rAn09shJgEjXaGIz746atZPCihcgCclYvLAE8MtyhWRJZz9zx1EKLSbxwtTwl6Hq_rX2ToPkt4GgYIbgffUYv.1IQ0eMCfdg794laY1YCSHQ" />
+            <input type="hidden" id="secondVisitUrl" value="&#x2f;ginasso&#x2f;SSORedirect&#x2f;metaAlias&#x2f;icpsem&#x3f;ReqID&#x3d;_519a347ee758ca27e3df73d68f7f30cf11c2bb1359&amp;index&#x3d;null&amp;acsURL&#x3d;https&#x3a;&#x2f;&#x2f;edu.ge.ch&#x2f;moodle&#x2f;auth&#x2f;saml2&#x2f;sp&#x2f;saml2-acs.php&#x2f;edu.ge.ch&amp;spEntityID&#x3d;https&#x3a;&#x2f;&#x2f;edu.ge.ch&#x2f;moodle&#x2f;auth&#x2f;saml2&#x2f;sp&#x2f;metadata.php&amp;binding&#x3d;urn&#x3a;oasis&#x3a;names&#x3a;tc&#x3a;SAML&#x3a;2.0&#x3a;bindings&#x3a;HTTP-POST" />
+            <input type="hidden" id="loginUrl" value="https&#x3a;&#x2f;&#x2f;ssoeel.geneveid.ch&#x3a;443&#x2f;ginasso&#x2f;UI&#x2f;Login&#x3f;realm&#x3d;&#x2f;&amp;spEntityID&#x3d;https&#x3a;&#x2f;&#x2f;edu.ge.ch&#x2f;moodle&#x2f;auth&#x2f;saml2&#x2f;sp&#x2f;metadata.php&amp;service&#x3d;eelService&amp;goto&#x3d;https&#x3a;&#x2f;&#x2f;ssoeel.geneveid.ch&#x3a;443&#x2f;ginasso&#x2f;saml2&#x2f;continue&#x2f;metaAlias&#x2f;icpsem&#x3f;secondVisitUrl&#x25;3D&#x2f;ginasso&#x2f;SSORedirect&#x2f;metaAlias&#x2f;icpsem&#x3f;ReqID&#x25;253D_519a347ee758ca27e3df73d68f7f30cf11c2bb1359&amp;ETATSSOAUTH_EELPROD&#x3d;" />
+        </form>
+    </body>
+</html>
